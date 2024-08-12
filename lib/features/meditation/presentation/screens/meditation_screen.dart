e6d76319de52efc6ba87f7fba8bef887ce548c72
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-12 23:17:01 +0530

    meditation screen completed

--- lib/features/meditation/presentation/screens/meditation_screen.dart
+++ lib/features/meditation/presentation/screens/meditation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_care_app/core/theme.dart';
 import 'package:mental_health_care_app/features/meditation/presentation/widgets/feeling_button.dart';
+import 'package:mental_health_care_app/features/meditation/presentation/widgets/task_card.dart';
 
 class MeditationScreen extends StatelessWidget {
   const MeditationScreen({super.key});
@@ -24,60 +25,90 @@ class MeditationScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          children: [
-            Text(
-              "Welcome back",
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const Text(
-              "How are you feeling today ?",
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FeelingButton(
-                  label: 'Happy',
-                  image: 'assets/happy.png',
-                  color: DefaultColors.pink,
-                  onTap: () {},
-                ),
-                FeelingButton(
-                  label: 'Calm',
-                  image: 'assets/calm.png',
-                  color: DefaultColors.purple,
-                  onTap: () {},
-                ),
-                FeelingButton(
-                  label: 'Relax',
-                  image: 'assets/relax.png',
-                  color: DefaultColors.orange,
-                  onTap: () {},
-                ),
-                FeelingButton(
-                  label: 'Focus',
-                  image: 'assets/focus.png',
-                  color: DefaultColors.lightteal,
-                  onTap: () {},
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Text(
-              'Today\'s Task',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              Text(
+                "Welcome back",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Text(
+                "How are you feeling today ?",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FeelingButton(
+                    label: 'Happy',
+                    image: 'assets/happy.png',
+                    color: DefaultColors.pink,
+                    onTap: () {},
+                  ),
+                  FeelingButton(
+                    label: 'Calm',
+                    image: 'assets/calm.png',
+                    color: DefaultColors.purple,
+                    onTap: () {},
+                  ),
+                  FeelingButton(
+                    label: 'Relax',
+                    image: 'assets/relax.png',
+                    color: DefaultColors.orange,
+                    onTap: () {},
+                  ),
+                  FeelingButton(
+                    label: 'Focus',
+                    image: 'assets/focus.png',
+                    color: DefaultColors.lightteal,
+                    onTap: () {},
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                'Today\'s Task',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TaskCard(
+                title: 'Morning',
+                description:
+                    "Let's open up to the thing that matters among the people",
+                color: DefaultColors.task1,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TaskCard(
+                title: 'Noon',
+                description:
+                    "Let's open up to the thing that matters among the people",
+                color: DefaultColors.task2,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TaskCard(
+                title: 'Evening',
+                description:
+                    "Let's open up to the thing that matters among the people",
+                color: DefaultColors.task3,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/meditation/presentation/widgets/task_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final Color color;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(12)),
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 5,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Text(
+                        description,
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                    ),
+                    const SizedBox(height: 3),
+                    // Container(
+                    //   padding: EdgeInsets.all(3),
+                    //   decoration: BoxDecoration(
+                    //     color: Colors.grey[200],
+                    //     borderRadius: BorderRadius.circular(50)
+                    //   ),
+                    //   child: Icon(Icons.arrow_forward_ios),
+                    // )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
