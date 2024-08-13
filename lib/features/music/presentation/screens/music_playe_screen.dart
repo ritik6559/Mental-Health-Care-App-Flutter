4bee10cfde55726647a81ad16d7d9553524aadb4
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-13 21:25:13 +0530

    adding music player screen

---
+++ lib/features/music/presentation/screens/music_playe_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayeScreen extends StatelessWidget {
+  const MusicPlayeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
