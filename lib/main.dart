7201301bb26272635633d2c2828f1fd2a2bcb618
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-17 23:27:46 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mental_health_care_app/core/theme.dart';
+import 'package:mental_health_care_app/features/meditation/presentation/bloc/get_daily_quote/get_daily_quote_bloc.dart';
+import 'package:mental_health_care_app/features/meditation/presentation/bloc/get_daily_quote/get_daily_quote_event.dart';
+import 'package:mental_health_care_app/features/meditation/presentation/bloc/mood_message/modd_message_state.dart';
 import 'package:mental_health_care_app/features/music/data/datasources/song_remote_data_sources.dart';
 import 'package:mental_health_care_app/features/music/data/repository/song_repository_impl.dart';
 import 'package:mental_health_care_app/features/music/domain/usecase/get_all_songs.dart';
@@ -8,8 +11,11 @@ import 'package:mental_health_care_app/features/music/presentation/bloc/song_blo
 import 'package:mental_health_care_app/presentation/bottomNavBar/bloc/navigation_bloc.dart';
 import 'package:mental_health_care_app/presentation/home/screens/home_screen.dart';
 import 'package:http/http.dart' as http;
+import 'init_dependencies.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -24,18 +30,16 @@ class MyApp extends StatelessWidget {
           create: (_) => NavigationBloc(),
         ),
         BlocProvider(
-          create: (context) => SongBloc(
-            getAllSongs: GetAllSongs(
-              SongRepositoryImpl(
-                songRemoteDataSources: SongRemoteDataSourcesImpl(
-                  client: http.Client(),
-                ),
-              ),
-            ),
-          )..add(
+          create: (context) => di.sl<SongBloc>()..add(
               FetchSongs(),
-            ),//fetching songs as soon as the playlist screen loads.
+            ), //fetching songs as soon as the playlist screen loads.
         ),
+        BlocProvider(
+            create: (context)=> di.sl<DailyQuoteBloc>()..add(FetchDailyQuote()),
+          ),
+          BlocProvider(
+              create: (context)=> di.sl<MoodMessageBloc>()
+          )
       ],
       child: MaterialApp(
         title: 'Mental Health App',
